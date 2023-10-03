943dbee9819b294ff22cbea5686020d1bef7651c
Lara Amadeo <laraamadeo@Toqios-MacBook-Pro.local>
2023-10-03 19:53:31 +0200

    fix colors static darkmode

--- web/src/modules/ExperienceItem.jsx
+++ web/src/modules/ExperienceItem.jsx
@@ -3,7 +3,7 @@ import Tag from "../components/Tag";
 export default function ExperienceItem({ date, title, description, tags }) {
     return <>
         <div className=" xl:gap-6 xl:grid xl:justify-start xl:mb-8 xl:grid-cols-8 flex flex-col gap-2 items-start mb-10">
-            <p className="text-xs whitespace-nowrap pt-1 col-span-2">{date}</p>
+            <p className="text-xs whitespace-nowrap pt-1 col-span-2 body-text-color">{date}</p>
             <div className="col-span-6">
 
                 <p className="text-start text-base font-medium pb-1 title-text-color">{title}</p>
